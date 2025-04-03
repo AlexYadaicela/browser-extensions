583e2cfe92f833ea7b58c53d44416ed8c4ac5816
spycrab <wialex121@gmail.com>
2025-04-03 16:33:29 -0400

    commit all files

--- main.js
+++ main.js
@@ -17,3 +17,84 @@ toggleColorScheme.addEventListener('click', () => {
             break; 
     }
 });
+
+
+
+const toggleExtensions = document.querySelectorAll('menu li button');
+
+toggleExtensions.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        const curr = document.getElementsByClassName('active'); 
+        if(curr.length > 0){
+            curr[0].classList.remove('active'); 
+        }
+
+        toggleExtensions[index].classList.add('active'); 
+    });
+});
+
+
+// remove extension item
+const removeButtons = document.querySelectorAll('.extension-controls button'); 
+const extensionItem = document.querySelectorAll('.extension-item'); 
+removeButtons.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        extensionItem[index].classList.add('is-removed'); 
+        extensionItem[index].style.display = 'none'; 
+    });
+});
+
+
+const extensionStatus = document.querySelectorAll('input[type="checkbox"');
+
+const showAll = document.getElementById('show-all'); 
+const showActive = document.getElementById('show-active'); 
+const showInactive = document.getElementById('show-inactive'); 
+
+
+showAll.addEventListener('click', () => {
+    extensionItem.forEach((e) => {
+        if(!e.classList.contains('is-removed')){
+            e.style.display = 'grid'; 
+        }
+    });
+});
+
+
+showActive.addEventListener('click', () => {
+    const inactiveElements = document.querySelectorAll('.view-inactive'); 
+    const activeElements = document.querySelectorAll('.view-active'); 
+    inactiveElements.forEach((e) => {
+        e.style.display = 'none'; 
+    }); 
+
+    activeElements.forEach((e) => {
+        e.style.display = 'grid'; 
+    });
+});
+
+showInactive.addEventListener('click', () => {
+    const inactiveElements = document.querySelectorAll('.view-inactive'); 
+    const activeElements = document.querySelectorAll('.view-active'); 
+    inactiveElements.forEach((e) => {
+        e.style.display = 'grid'; 
+    }); 
+
+    activeElements.forEach((e) => {
+        e.style.display = 'none'; 
+    });
+});
+
+extensionStatus.forEach((btn, index) => {
+    extensionItem[index].classList.add('view-inactive'); 
+    btn.addEventListener('change', () => {
+        if(btn.checked){
+            extensionItem[index].classList.add('view-active');
+            extensionItem[index].classList.remove('view-inactive'); 
+        }else{
+            extensionItem[index].classList.remove('view-active'); 
+            extensionItem[index].classList.add('view-inactive');
+        }
+    });
+});
+
